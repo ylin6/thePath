60cd2172bfe6fa4714d985c3622ca0b13aa6bb2b
Marshall <msprigg@nd.edu>
2015-05-06 17:51:59 -0400

    Pmessage fixes

--- src/pmessage.py
+++ src/pmessage.py
@@ -2,6 +2,7 @@
 #Class for sending player data between the clients
 
 class PlayerMessage:
-	def __init__(self, xcoord, ycoord):
+	def __init__(self, xcoord, ycoord, img):
 		self.xPos = xcoord
 		self.yPos = ycoord
+		self.image = img
